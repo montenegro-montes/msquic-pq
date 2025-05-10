743bef7f6e00c3ce5dd40bfb2b6b858f25a39f51
José Antonio Montenegro Montes <126468995+montenegro-montes@users.noreply.github.com>
2025-05-10 22:31:24 +0200

    Update s_client.c

--- src/tools/s_client/s_client.c
+++ src/tools/s_client/s_client.c
@@ -27,7 +27,7 @@ uint16_t UdpPort = 4433;
 //
 // The default idle timeout period (1 second) used for the protocol.
 //
-const uint64_t IdleTimeoutMs = 1000;
+const uint64_t IdleTimeoutMs = 120000;
 
 //
 // The length of buffer sent over the streams in the protocol.
