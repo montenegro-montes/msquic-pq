5b531d95dbbccf26ee2a0eb1bf7e87b3adc5306f
José Antonio Montenegro Montes <126468995+montenegro-montes@users.noreply.github.com>
2025-05-10 18:08:41 +0200

    Update s_connection.c

--- src/tools/s_connection/s_connection.c
+++ src/tools/s_connection/s_connection.c
@@ -15,7 +15,8 @@
 // the app (used for persistent storage and for debugging). It also configures
 // the execution profile, using the default "low latency" profile.
 //
-const QUIC_REGISTRATION_CONFIG RegConfig = { "quics_connection", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+#const QUIC_REGISTRATION_CONFIG RegConfig = { "quics_connection", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+const QUIC_REGISTRATION_CONFIG RegConfig = { "quics_connection", QUIC_EXECUTION_PROFILE_TYPE_REAL_TIME };
 
 //
 // The protocol name used in the Application Layer Protocol Negotiation (ALPN).
